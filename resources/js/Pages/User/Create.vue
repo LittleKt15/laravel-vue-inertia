70220f03962e4b31e0a92b7901ce1759db0a61f7
KyawZin <hellfirewarrior123@gmail.com>
2023-10-05 01:21:11 +0630

    'kzt'

---
+++ resources/js/Pages/User/Create.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="container mx-auto px-5 mt-5">
+        <div class="text-3xl mb-5">Create User Form</div>
+
+        <div class="grid md:grid-cols-3">
+            <div>
+                <form @submit.prevent="submit">
+                    <div>
+                        <label for="first_name" class="block mb-2 text-md font-medium">Name</label>
+                        <input type="text" id="first_name" v-model="form.name"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                    </div>
+                    <div>
+                        <label for="email" class="block mb-2 text-md font-medium">Email</label>
+                        <input type="text" id="email" v-model="form.email"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                    </div>
+                    <div class="mb-6">
+                        <label for="password" class="block mb-2 text-md font-medium">Password</label>
+                        <input type="password" id="password" v-model="form.password"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                    </div>
+                    <Link href="/users"
+                        class="inline-block text-white bg-gray-600 hover:bg-gray-700 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2">
+                    Back</Link>
+                    <button type="submit"
+                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Create</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            form: {
+                name: '',
+                email: '',
+                password: '',
+            }
+        }
+    },
+    methods: {
+        submit() {
+            this.$inertia.post('/users/create', this.form)
+        }
+    }
+}
+</script >
+
+<script setup>
+import { Link } from '@inertiajs/vue3'
+</script>
+
+<style lang="scss" scoped></style>
---
+++ resources/js/Pages/User/Index.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="container mx-auto px-5 mt-5">
+        <div class="flex justify-between">
+            <div class="text-3xl mb-5">User List</div>
+            <div>
+                <Link href="/users/create"
+                    class="inline-block text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:ring-blue-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Create</Link>
+            </div>
+        </div>
+
+        <div class="relative overflow-x-auto">
+            <div v-if="$page.props.add" class="p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50" role="alert">
+                <span class="font-medium text-lg">{{ $page.props.add }}</span>
+            </div>
+            <div v-if="$page.props.del" class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50" role="alert">
+                <span class="font-medium text-lg">{{ $page.props.del }}</span>
+            </div>
+            <table class="w-full text-sm text-left text-gray-500">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-300">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">
+                            ID
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Name
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Email
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Date
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Action
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="user in users" :key="user.id" class="bg-white border-b">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                            {{ user.id }}
+                        </th>
+                        <td class="px-6 py-4">
+                            {{ user.name }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ user.email }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ formatDate(user.created_at) }}
+                        </td>
+                        <td class="px-6 py-4">
+                            <a href=""
+                                class="text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:ring-green-200 font-medium rounded-lg text-sm px-5 py-2.5">Edit</a>
+                            <button @click="destroy(user.id)"
+                                class="text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:ring-red-200 font-medium rounded-lg text-sm px-5 py-2.5 ml-1">Delete</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+    </div>
+</template>
+
+<script>
+import { format } from 'date-fns';
+
+export default {
+    // props: ['del'],
+    methods: {
+        destroy(id) {
+            this.$inertia.delete(`/users/${id}`);
+        },
+        formatDate(date) {
+            // return format(new Date(date), 'yyyy-MM-dd HH:mm:ss');
+            return format(new Date(date), 'yyyy-MM-dd');
+        }
+    }
+}
+</script>
+
+<script setup>
+import { Link   } from '@inertiajs/vue3'
+
+defineProps({ users: Object })
+</script>
+
+<style lang="scss" scoped></style>
